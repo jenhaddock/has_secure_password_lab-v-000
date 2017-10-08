48494e88a773bc2bc88adf0593593c2536566985
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 04:15:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:name])
     if !@user.nil? && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_path
     else
       redirect_to 'login'
     end
