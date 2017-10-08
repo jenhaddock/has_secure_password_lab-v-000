6c90898a3c52ec30a6752e2463e0df8788181874
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 02:38:00 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to "/"
     else
       render :new
     end
