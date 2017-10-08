41e9f847d8001f3b7046f4b3a47af48646bd1054
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 03:49:26 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       render :homepage
     else
       render :new
