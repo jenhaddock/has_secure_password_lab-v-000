5515fd95088e2c978e1e2df3e85c64ffe685023d
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 03:15:08 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
