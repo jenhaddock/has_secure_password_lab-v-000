b6790c3732a1b3fcacdd4fdc5d8f0e40e17980f2
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 04:17:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    binding.pry
     if !@user.nil? && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
