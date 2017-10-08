bcecc36e9e2673df206c046a6f95cba22866559b
Jennifer Haddock <jentx123@hotmail.com>
2017-10-08 02:23:55 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :require_login
 
+  def create
+
+  end
+
   def require_login
     redirect_to '/login' unless session.include? :name
   end
